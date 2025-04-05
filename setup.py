e9c76391af9d8a8e1196985f5bc004ffd0cf3de4
amirrezadadi <amirreza.dadfarnia@tap30.net>
2025-04-05 15:21:23 +0330

    rename main directory to litetoollm

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="litetoollm",
     version="0.1.0",
-    packages=["structurallm"],
+    packages=["litetoollm"],
     py_modules=["__init__"],
     install_requires=[
         "litellm>=1.30.7",
